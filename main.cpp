bf957612603b8f08635072ea04d3bfa482b7810c
Carlton Semple <CarltonSemple@users.noreply.github.com>
2015-02-01 01:48:32 -0500

    reformatted code

--- main.cpp
+++ main.cpp
@@ -1,45 +1,37 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
-//#include <cstdio.h>
 
 // Read function
 byte read() {
-    
-    // flag goes to 1 upon receiving
-    // loop until it receives
-    if(SCI1SR1_RDRF != 1)
-        return 0;
-    
-    // return the data
-    return SCI1DRL;
-    
+	// flag goes to 1 upon receiving
+	// loop until it receives
+	if(SCI1SR1_RDRF != 1)
+		return 0;
+	
+	// return the data
+	return SCI1DRL;
 }
 
 void transmit(byte data){
-    while(SCI1SR1_TDRE != 1)
-        ;
-        
-    SCI1DRL = data;  
+	while(SCI1SR1_TDRE != 1);
+	SCI1DRL = data;  
 }
 
 // set up SPI control register 1
-void setup_SPI1(){
-     
-  SPI0CR1_SPE = 1;  // Enable SPI 1
-  SPI0CR1_MSTR = 1; // make microcrontroller the master
-  SPI0CR1_CPOL = 1; // polarity
-  SPI0CR1_CPHA = 0; // phase
-  SPI0CR1_SSOE = 1; // slave select output enable 
-  SPI0CR2_MODFEN = 1; // Mode Fault Enable Bit
-  SPI0CR2_XFRW = 1; // 1 = 16-bit 
+void setup_SPI1()
+{
+	SPI0CR1_SPE = 1;  // Enable SPI 1
+	SPI0CR1_MSTR = 1; // make microcrontroller the master
+	SPI0CR1_CPOL = 1; // polarity
+	SPI0CR1_CPHA = 0; // phase
+	SPI0CR1_SSOE = 1; // slave select output enable 
+	SPI0CR2_MODFEN = 1; // Mode Fault Enable Bit
+	SPI0CR2_XFRW = 1; // 1 = 16-bit 
 }
 
-
 void startTimer(int delayCount){
-   TC0 = TC0 + delayCount; // count this many cycles
-   while(!TFLG1_C0F);  // loop while the timer isn't finished
-   
-   //TSCR1_TEN = 0; // disable timer
+	TC0 = TC0 + delayCount; // count this many cycles
+	while(!TFLG1_C0F);  // loop while the timer isn't finished
 }
 
 // Convert character array to integer
@@ -80,7 +72,7 @@ int algorithm_1_100_hz(int inputNum)
 
 int algorithm_100hz_1khz(int inputNum)
 {
-   return (inputNum - 100) / 10;
+	return (inputNum - 100) / 10;
 }
 
 
@@ -110,172 +102,132 @@ static byte squareLookup[35] = {  255, 255, 255, 255, 255, 255, 255, 255, 255, 2
       
 
 void main(void) {
-  
-  // to begin, 
-  byte i = 0;
-  byte divisor, count =0 , subtractor;
-  char receivedData;  // for reading from realterm
-  byte letter=97, test;
-  int userInput = 1; // the input # from the user
-  int three, two, one;
-  //double forty = 40000000, thirtyfour = 34;
-  //int timer = 1;
-  byte temp = -1;
-  byte inverse;
-  
-   TIOS = 0x01; // set timers to output
-  
-   TSCR1_TEN = 1; // enable timer
-   TSCR1_TFFCA = 1; // clear
-   // delay amount
-   
-    TSCR2_PR2 = 1;  // enable a smaller measurement. dividing by 16   
-    TSCR2_PR1 = 0;      
-    TSCR2_PR0 = 1;      
+	// to begin, 
+	byte i = 0;
+	byte divisor, count =0 , subtractor;
+	char receivedData;  // for reading from realterm
+	byte letter=97, test;
+	int userInput = 1; // the input # from the user
+	int three, two, one;
+	//double forty = 40000000, thirtyfour = 34;
+	//int timer = 1;
+	byte temp = -1;
+	byte inverse;
+	
+	TIOS = 0x01; // set timers to output
+	
+	TSCR1_TEN = 1; // enable timer
+	TSCR1_TFFCA = 1; // clear
+	// delay amount
+	
+	TSCR2_PR2 = 1;  // enable a smaller measurement. dividing by 16   
+	TSCR2_PR1 = 0;      
+	TSCR2_PR0 = 1;      
                          
-  // set up SPI 0
-  setup_SPI1();
-  
-  // Receiving data ********************
-  // set baud rate
-  SCI1BDH = 0x00;
-  SCI1BDL = 0x28;
-  //SCI1BD = 260;
-  // Enable Receive and transmission
-  SCI1CR2_RE = 1;
-  SCI1CR2_TE = 1; // enable
-                              
-  // set MODRR to 0x50 (Port M)
-  MODRR = 0x50;
-  
-   
-  // EXTRAS   Speedup
-  
-  SYNR = 0x49;
-  REFDV = 0x42;
-  while(!CRGFLG_LOCK)
-      ;
-  // wait for lock
-  //CLKSEL_PLLSEL = 1; // clock select register's highest bit
-   CLKSEL = 0x80;
-    
-
-  for(;;) {
-    
-
-    test = read(); // letter
-    if(test != 0){
-        if((test != 97) && (test != 115) && (test != 100)) {
-           temp = test;
-        } 
-        else{
-          temp = -1;
-          letter = test;
-        }
-    }
-
-    if(temp == 49){ // 1 - decrement by 10
-        userInput = userInput - 10;
-        temp = 0;
-    }
-    
-    if(temp == 50){ // 2 - increment by 10
-        userInput = userInput + 10;
-        temp = 0;
-    }
-    
-    if(temp == 51){  // 3 - decrement by 1
-        userInput = userInput - 1;
-        temp = 0;      
-    }
-    
-    if(temp == 52){  // 4 - increment by 1
-        userInput = userInput + 1;
-        temp = 0;      
-    }
-    
-    if(temp == 53){  // 5 - decrement by 100
-        userInput = userInput - 100;
-        temp = 0;      
-    }
-    
-    if(temp == 54){  // 6 - increment by 100
-        userInput = userInput + 100;
-        temp = 0;      
-    }
+	// set up SPI 0
+	setup_SPI1();
+	
+	// Receiving data ********************
+	// set baud rate
+	SCI1BDH = 0x00;
+	SCI1BDL = 0x28;
+	//SCI1BD = 260;
+	// Enable Receive and transmission
+	SCI1CR2_RE = 1;
+	SCI1CR2_TE = 1; // enable
+	                      
+	// set MODRR to 0x50 (Port M)
+	MODRR = 0x50;
   
    
+	// EXTRAS   Speedup
+	
+	SYNR = 0x49;
+	REFDV = 0x42;
+	while(!CRGFLG_LOCK);
+	
+	// wait for lock
+	//CLKSEL_PLLSEL = 1; // clock select register's highest bit
+	CLKSEL = 0x80;
+    
 
-    /*
-    if(userInput < 100) {
-    // 1 to 10Hz & 10 Hz to 100 Hz
-    TSCR2_PR2 = 1;  // enable a smaller measurement. dividing by 16   
-    TSCR2_PR1 = 1;      // divide by  . both are needed for 1 to 10Hz
-    TSCR2_PR0 = 0;
-    }
-    else {
-      // 100 to 1kHz
-      TSCR2_PR2 = 1;  // enable a smaller measurement. dividing by 16   
-      TSCR2_PR1 = 0; 
-      TSCR2_PR0 = 1;
-    }
-       */
-    //letter = parseInput(&userInput); 
-    // wait for flag
-    /*while(SPI0SR_SPTEF != 1);
+	for(;;) 
+	{
+		test = read(); // letter
+		if(test != 0)
+		{
+			if((test != 97) && (test != 115) && (test != 100)) 
+		   		temp = test;
+			else
+			{
+				temp = -1;
+				letter = test;
+			}
+		}
+
+		if(temp == 49){ // 1 - decrement by 10
+			userInput = userInput - 10;
+			temp = 0;
+		}
+		
+		if(temp == 50){ // 2 - increment by 10
+			userInput = userInput + 10;
+			temp = 0;
+		}
     
-    SPI0DR = 0x0F00 +  sineLookup[i]; // delay + offset
-      i++;
-      if(i==33)
-        i=0;
-            */
+		if(temp == 51){  // 3 - decrement by 1
+			userInput = userInput - 1;
+			temp = 0;      
+		}
     
-      
-    //startTimer(100);
-    //startTimer(50);
-    
-    
-    
-    while(SPI0SR_SPTEF != 1);
+		if(temp == 52){  // 4 - increment by 1
+			userInput = userInput + 1;
+			temp = 0;      
+		}
+		
+		if(temp == 53){  // 5 - decrement by 100
+			userInput = userInput - 100;
+			temp = 0;      
+		}
+		
+		if(temp == 54){  // 6 - increment by 100
+			userInput = userInput + 100;
+			temp = 0;      
+		}
+
+		while(SPI0SR_SPTEF != 1);
      
-    if(letter == 97){
-     // a: Sine ********************************************
-    // Give the SPI data register any command + data
-      // SEND
-      SPI0DR = 0x0F00 +  sineLookup[i]; // delay + offset
-      i++;
-      if(i==33)
-        i=0; 
-    } 
-    else if(letter == 115){
-    // s: Square ********************************************
-      // Give the SPI data register any command + data
-      // SEND
-      SPI0DR = 0x0F00 +  squareLookup[i]; // delay + offset
-      i++;
-      if(i==34)
-        i=0;
-    }
-    else if(letter == 100) {
-    // d: Triangle ********************************************
-      // Give the SPI data register any command + data
-      // SEND
-      SPI0DR = 0x0F00 +  triangleLookup[i]; // delay + offset
-      i++;
-      if(i==35)
-        i=0;
-    }
-        
-   startTimer(36764/userInput); 
-  
-    
-  
-  
-  //EnableInterrupts;
-  
-
-    
-    
-  } /* loop forever */
-  /* please make sure that you never leave main */
+		if(letter == 97)
+		{
+			// a: Sine ********************************************
+			// Give the SPI data register any command + data
+			// SEND
+			SPI0DR = 0x0F00 +  sineLookup[i]; // delay + offset
+			i++;
+			if(i==33)
+			i=0; 
+		} 
+		else if(letter == 115)
+		{
+			// s: Square ********************************************
+			// Give the SPI data register any command + data
+			// SEND
+			SPI0DR = 0x0F00 +  squareLookup[i]; // delay + offset
+			i++;
+			if(i==34)
+			i=0;
+		}
+		else if(letter == 100) 
+		{
+			// d: Triangle ********************************************
+			// Give the SPI data register any command + data
+			// SEND
+			SPI0DR = 0x0F00 +  triangleLookup[i]; // delay + offset
+			i++;
+			if(i==35)
+			i=0;
+		}
+		
+		startTimer(36764/userInput); 
+	} /* loop forever */
 }
